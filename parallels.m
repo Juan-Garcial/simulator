86a8a113217cb7d6a14363ebdec74c203b514076
juan garcia <jcarlos,garcia@alumnos.udg.mx>
2022-10-17 17:32:22 -0500

    parallels works for n components

--- parallels.m
+++ parallels.m
@@ -3,29 +3,31 @@ function [coordenadas,Y] = parallels(coordenadas,Y)
     tamano = size(Y);
     tamano = tamano(1,1);
     entry = false;
-    for i=1:tamano-1
-        
-        if(entry)
+    i = 1;
+    while i<tamano
+    
+        j = i+1;
 
-            i = i-1;
-            entry = false;
-                
-        end
-
-        for j=i+1:tamano
+        while j<=tamano
 
             if((coordenadas(j,1)== coordenadas(i,1) & coordenadas(j,2)== coordenadas(i,2)) | (coordenadas(j,2)== coordenadas(i,1) & coordenadas(j,1)== coordenadas(i,2)) )
 
                 Y(i,1) = Y(i,1)+Y(j,1);
                 Y(j, :) =[];
                 coordenadas(j, :) = [];
-                entry = true;
                 tamano = size(Y);
                 tamano = tamano(1,1);
+                
+            else
 
-            
+                j = j+1;
+
             end
+
         end
+        
+        i = i+1;
+
     end
 end
 
